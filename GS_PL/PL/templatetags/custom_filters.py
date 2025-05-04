158e7a4edb767d17f1a6f215702bdd907e7b493e
dsyi23g <a02251323@gmail.com>
2025-05-04 21:58:25 +0100

    Delete GS_PL directory
    Signed-off-by: dsyi23g <a02251323@gmail.com>

--- GS_PL/PL/templatetags/custom_filters.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def until(value, end):
-    return range(value, end)
--- GS_PL/PL/templatetags/custom_tags.py
+++
@@ -1,8 +0,0 @@
-# PL/templatetags/custom_tags.py
-from django import template
-
-register = template.Library()
-
-@register.filter
-def attr(obj, attr_name):
-    return getattr(obj, attr_name, '')
